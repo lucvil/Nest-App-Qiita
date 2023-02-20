31c5176dd2d687dda1c1d5c7dc3b28bda1d4bf11
lucvil <yoshimura037@g.ecc.u-tokyo.ac.jp>
2023-02-20 11:55:43 +0900

    ~p293

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param, Body, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import FormData from './formdata';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/hello/:id?')
+  getHello(@Param() params): object {
+    const id = params.id?? 0;
+    return this.appService.getHello(id);
+  }
+
+  @Post('/hello/post')
+  post(@Body() frm: FormData): string {
+    this.appService.addData(frm);
+    return 'form data was pushed';
   }
 }
